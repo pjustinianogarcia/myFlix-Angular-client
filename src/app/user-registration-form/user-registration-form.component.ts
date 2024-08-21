38276f723b0534a3434935deea0bc7998fcfbafa
pjustinianogarcia <158845576+pjustinianogarcia@users.noreply.github.com>
2024-08-21 10:41:49 -0400

    Fixed buttons

--- src/app/user-registration-form/user-registration-form.component.ts
+++ src/app/user-registration-form/user-registration-form.component.ts
@@ -30,6 +30,7 @@ ngOnInit(): void {
 registerUser(): void {
   this.fetchApiData.userRegistration(this.userData).subscribe((result) => {
 // Logic for a successful user registration goes here! (To be implemented)
+console.log(result)
    this.dialogRef.close(); // This will close the modal on success!
    this.snackBar.open(result, 'OK', {
       duration: 2000
