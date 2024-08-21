6cc80b91844bededf94623a00ca06946cde4c98b
pjustinianogarcia <158845576+pjustinianogarcia@users.noreply.github.com>
2024-08-20 23:55:26 -0400

    Update fetch-api-data.service.ts

--- src/app/fetch-api-data.service.ts
+++ src/app/fetch-api-data.service.ts
@@ -5,7 +5,7 @@ import { Observable, throwError } from 'rxjs';
 import { map } from 'rxjs/operators';
 
 //Declaring the api url that will provide data for the client app
-const apiUrl = 'https://movie-api-3jxi.onrender.com';
+const apiUrl = 'https://movie-api-3jxi.onrender.com/';
 @Injectable({
   providedIn: 'root'
 })
