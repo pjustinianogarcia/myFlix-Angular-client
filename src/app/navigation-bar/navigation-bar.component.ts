4279ef5c298ed030d7149fcf93f83fba9118eca5
pjustinianogarcia <158845576+pjustinianogarcia@users.noreply.github.com>
2024-09-02 23:55:59 -0400

    Added log out logic

--- src/app/navigation-bar/navigation-bar.component.ts
+++ src/app/navigation-bar/navigation-bar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navigation-bar',
@@ -6,5 +7,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./navigation-bar.component.scss']
 })
 export class NavigationBarComponent {
-
+  constructor(private router: Router) {}
+  /**
+   * Checks if the current route is the welcome page.
+   * 
+   * @returns {boolean} - Returns true if the current route is not the welcome page.
+   */
+  shouldDisplayNavBar(): boolean {
+    return this.router.url !== '/welcome'; // Adjust the route if your welcome route is different
+  }
+   /**
+   * Logs out the user by clearing their data from localStorage and redirecting to the welcome page.
+   * 
+   * @returns {void}
+   */
+   logout(): void {
+    localStorage.clear(); // Clear user data from localStorage
+    this.router.navigate(['/welcome']); // Redirect to the welcome page
+  }
 }
